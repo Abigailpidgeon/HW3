1d3bb6ad5b66fdbd510f770f41ad67953cd9fa7c
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 09:45:12 -0500

    Update appointments-by-procedure.php

--- appointments-by-procedure.php
+++ appointments-by-procedure.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-appointments-by-procedure.php");
 
+$pageTitle = "Appointments by Procedure";
+include "veiw-header.php";
+$courses = selectAppointmentsbyProcedure($_POST['pid']);
+include "veiw-appointment-by-procedures.php";
+include "veiw-footer.php";
+?>
