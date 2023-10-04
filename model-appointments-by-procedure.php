860be63f4483916e24135244291d099968d3aa00
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 13:42:06 -0500

    Update model-appointments-by-procedure.php

--- model-appointments-by-procedure.php
+++ model-appointments-by-procedure.php
@@ -2,7 +2,7 @@
 function selectAppointmentsbyProcedure($pid) {
    try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.procedure_id, procedure_name, procedure_desc, room, day_time FROM `procedure` p join appointment a on a.procedure_id = p.procedure_id where p.doctor_id=?");
+        $stmt = $conn->prepare("SELECT p.procedure_id, procedure_name, procedure_desc, room, day_time FROM `procedure` p join appointment a on a.procedure_id = p.procedure_id where a.doctor_id=?");
         $stmt->bind_param("i",$pid);
         $stmt->execute();
         $result = $stmt->get_result();
