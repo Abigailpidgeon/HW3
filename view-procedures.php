651813518ba23c13bdcc3f7b23f3f9a8a8dac7d4
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 09:19:27 -0500

    Update view-procedures.php

--- view-procedures.php
+++ view-procedures.php
@@ -1,1 +1,26 @@
-
+<h1>Procedures</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+        <tr>
+      <th>ID</th>
+    <th>Name</th>
+    <th>Description</th>
+    </tr>
+    </thead>
+  </body>
+  <?php 
+while ($procedure = $procedures->fetchassoc())
+{
+  ?>
+  <tr>
+    <td><?php echo $procedure['procedure_id']; ?></td>
+    <td><?php echo $procedure['procedure_name']; ?></td>
+    <td><?php echo $procedure['procedure_desc']; ?></td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+</table>
+</div>
