7411f2661621f4a895adcefc653f91652d8bfd02
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 13:47:26 -0500

    Update appointments-by-procedure.php

--- appointments-by-procedure.php
+++ appointments-by-procedure.php
@@ -5,6 +5,6 @@ require_once("model-appointments-by-procedure.php");
 $pageTitle = "Appointments by Procedure";
 include "view-header.php";
 $procedure  = selectAppointmentsbyProcedure($_POST['pid']);
-include "view-appointment-by-procedures.php";
+include "view-appointment-by-procedure.php";
 include "view-footer.php";
 ?>
