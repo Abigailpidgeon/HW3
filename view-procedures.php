bdcb719b7fcb8fb06d4ea4260adfb85e010e08c0
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 09:57:44 -0500

    Update view-procedures.php

--- view-procedures.php
+++ view-procedures.php
@@ -8,7 +8,7 @@
     <th>Description</th>
     </tr>
     </thead>
-  </body>
+  <tbody>
   <?php 
 while ($procedure = $procedures->fetch_assoc())
 {
@@ -17,6 +17,12 @@ while ($procedure = $procedures->fetch_assoc())
     <td><?php echo $procedure['procedure_id']; ?></td>
     <td><?php echo $procedure['procedure_name']; ?></td>
     <td><?php echo $procedure['procedure_desc']; ?></td>
+  <td>
+    <form method = "post" action = "appointments-by-procedure.php">
+      <input type="hidden" name="pid" value="<?php echo $course['course_id']; ?>">
+      button type ="submit" class="btn btn-primary">Appointment</button>
+    </form>
+  </td>
   </tr>
   <?php
 }
