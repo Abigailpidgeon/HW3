bdc1170c7c7a74cdfa689ec40605923e01057ebd
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 13:52:54 -0500

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-appointments.php");
 
 $pageTitle = "Appointments";
-//include "view-header.php";
+include "view-header.php";
 $appointments = selectAppointments();
 include "view-appointments.php";
 include "view-footer.php";
