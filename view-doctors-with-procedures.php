9cafd95acedcf7d89350f64e9bff118dfe5c89b4
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 10:26:25 -0500

    Update view-doctors-with-procedures.php

--- view-doctors-with-procedures.php
+++ view-doctors-with-procedures.php
@@ -8,10 +8,15 @@ while ($doctor = $doctors->fetch_assoc())
     <div class ="card-body">
       <h5 class="card-title"><?php echo $doctor['doctor_name']; ?></h5>
         <p class="card-text">
-  <?php
-  $courses = selectProceduresbyDoctor
-}
-?>
+<?php
+function selectProceduresbyDoctor($doctor['doctor_id']);
+  while ($procedure = $procedures->fetch_assoc()) {
+    ?>
+    <li class = "list-group-item"><?php echo $procedure['procedure_id']; ?> - <?php echo $procedure['procedure_name']; ?> - <?php echo $procedure['procedure_desc']; ?> - <?php echo $procedure['room']; ?> - <?php echo $procedure['day_time']; ?>
+<?php
+  }
+  ?>
+    </ul>
         </p>
       <p class ="card-text"><small class="text-bpdy-secondary">Clinic: <?php echo $doctor['clinic_name']; ?></small></p>
 </div>
