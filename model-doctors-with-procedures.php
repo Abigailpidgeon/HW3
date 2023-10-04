a402ef947074f9dcd65ca6d3b72fee5ed891dd7c
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 10:06:18 -0500

    Update model-doctors-with-procedures.php

--- model-doctors-with-procedures.php
+++ model-doctors-with-procedures.php
@@ -13,3 +13,20 @@ function selectDoctors() {
     }
 }
 ?>
+
+<?php
+function selectProceduresbyDoctor($iid) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.procedure_id, procedure_name, procedure_desc, room, day_time FROM `procedure` p join appointment a on a.procedure_id = p.procedure_id where p.doctor_id=?");
+        $stmt->bind_param("i",$iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
