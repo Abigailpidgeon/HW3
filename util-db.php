8cfb4107d631cf4e8b1df3bcf3f334416333ff87
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 13:28:05 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('138.68.5.11', 'apidgeon_hw3user', 'Qyl=Jh=f[w3Q', 'apidgeon_HW3');
+    $conn = new mysqli('159.89.47.44', 'apidgeon_hw3user', 'Qyl=Jh=f[w3Q', 'apidgeon_HW3');
     
     // Check connection
     if ($conn->connect_error) {
