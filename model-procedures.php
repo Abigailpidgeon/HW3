b621e2f0f29f7bd5f4ee38ef5d9d861d7844eda1
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 09:14:51 -0500

    Create model-procedures.php

---
+++ model-procedures.php
@@ -0,0 +1,15 @@
+<?php
+function selectProcedures {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT procedure_id, procedure_name, procedure_desc FROM `procedure`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
