aaf4802c5f935ae6f07d316332bf94964970a6dc
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 10:13:46 -0500

    Create view-doctors-with-procedures.php

---
+++ view-doctors-with-procedures.php
@@ -0,0 +1,21 @@
+<h1>Doctors with Procedures</h1>
+<div class="card-group">
+<?php
+while ($doctor = $doctors->fetch_assoc())
+{
+  ?>
+  <div class="card">
+    <div class ="card-body">
+      <h5 class="card-title"><?php echo $doctor['doctor_name']; ?></h5>
+        <p class="card-text">
+  <?php
+}
+?>
+        </p>
+      <p class ="card-text"><small class="text-bpdy-secondary">Clinic: <?php echo $doctor['clinic_name']; ?></small></p>
+</div>
+  </div>
+<?php
+}
+?>
+</div>
