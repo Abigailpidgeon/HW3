1947e0cbeb64cabf655f0a023e7ccf34e864c257
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 13:37:46 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -33,6 +33,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="doctors-with-procedures.php">Doctors with Procedures</a>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="appointments-by-procedure">Appointments by Procedure</a>
         </li>
       </ul>
       </form>
