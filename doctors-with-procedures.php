ca9e93b2e158a2b39d6efe6c889deeb0a8d1aae6
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 10:02:53 -0500

    Update and rename docters-with-procedures.php to doctors-with-procedures.php

---
+++ doctors-with-procedures.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-doctors-with-procedures.php");
+
+$pageTitle = "Doctors with Procedure";
+include "veiw-header.php";
+$courses = selectDoctors();
+include "veiw-doctors-with-procedures.php";
+include "veiw-footer.php";
+?>
