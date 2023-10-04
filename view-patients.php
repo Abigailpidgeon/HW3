9d3b5f60a542e5a293cba42e19a1ca5f08410b7b
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 12:13:19 -0500

    Update view-patients.php

--- view-patients.php
+++ view-patients.php
@@ -1,1 +1,27 @@
-
+<h1>Patients</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+        <tr>
+      <th>ID</th>
+    <th>Name</th>
+    <th>Insurance</th>
+    </tr>
+    </thead>
+  <tbody>
+  <?php 
+while ($patient = $patients->fetch_assoc())
+{
+  ?>
+  <tr>
+    <td><?php echo $patient['patient_id']; ?></td>
+    <td><?php echo $patient['patient_name']; ?></td>
+    <td><?php echo $patient['insurance_name']; ?></td>
+  </td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+</table>
+</div>
