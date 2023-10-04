59fe2d535ab276dd131ca5652eba151bbfd1d9ad
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 09:52:18 -0500

    Update view-appointment-by-procedure.php

--- view-appointment-by-procedure.php
+++ view-appointment-by-procedure.php
@@ -1,1 +1,30 @@
-
+<h1>Appointments by Procedure</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+        <tr>
+      <th>ID</th>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Room</th>
+    <th>Day/Time</th>
+    </tr>
+    </thead>
+  <tbody>
+  <?php 
+while ($appointment = $appointments->fetch_assoc())
+{
+  ?>
+  <tr>
+    <td><?php echo $appointment['procedure_id']; ?></td>
+    <td><?php echo $appointment['procedure_name']; ?></td>
+    <td><?php echo $appointment['procedure_desc']; ?></td>
+    <td><?php echo $appointment['room']; ?></td>
+    <td><?php echo $appointment['day_time']; ?></td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+</table>
+</div>
