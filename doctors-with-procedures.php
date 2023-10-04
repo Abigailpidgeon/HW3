9b15bd859042f03e6847758a66fa8ffa8dcc9ea7
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-04 12:43:33 -0500

    Update doctors-with-procedures.php

--- doctors-with-procedures.php
+++ doctors-with-procedures.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-doctors-with-procedures.php");
 
 $pageTitle = "Doctors with Procedure";
-include "veiw-header.php";
+include "view-header.php";
 $doctors = selectDoctors();
-include "veiw-doctors-with-procedures.php";
-include "veiw-footer.php";
+include "view-doctors-with-procedures.php";
+include "view-footer.php";
 ?>
