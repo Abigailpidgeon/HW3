e4d82d630070991bb850100e7b5c6ebf1bd61611
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 18:16:11 -0500

    Update view-procedures.php

--- view-procedures.php
+++ view-procedures.php
@@ -1,4 +1,13 @@
-<h1>Procedures</h1>
+<div class="row">
+  <div class="col">
+<h1>Courses</h1>
+  </div>
+  <div class = "col-auto">
+  <?php
+    include "view-courses-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
