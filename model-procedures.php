9d173d2aa4efeaaef192deccd37bbad1d2d965cc
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 14:34:09 -0500

    Update model-procedures.php

--- model-procedures.php
+++ model-procedures.php
@@ -12,4 +12,17 @@ function selectProcedures() {
         throw $e;
     }
 }
+function insertProcedures($pName, $pDesc) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `procedure` (`procedure_name`, `procedure_desc`) VALUES (?,?);
+        $stmt->bind_param("ss",$pName, $pDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
