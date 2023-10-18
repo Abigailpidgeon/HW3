aa26acdac68bd20b9599cf9dd9dd8f3bf8412d7c
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 15:33:29 -0500

    Update model-patients.php

--- model-patients.php
+++ model-patients.php
@@ -12,4 +12,42 @@ function selectPatients() {
         throw $e;
     }
 }
+function insertPatients($patname, $inname) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `patient` (`patient_name`, `insurance_name`) VALUES (?,?);
+        $stmt->bind_param("ss",$patname, $inname);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}function updatePatients($patname, $inname, $patid) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `patient` set `patient_name` = ?, `insurance_name` = ?, where patient_id = ?");
+        $stmt->bind_param("ssi",$patname, $inname, $patid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+}function deletePatients($patid) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from patient where patient_id = ?");
+        $stmt->bind_param("i", $patid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
