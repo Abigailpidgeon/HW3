1bd9f75f625b7f5dd65e37b08193d27fd8f481e0
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 18:38:12 -0500

    Update view-doctors.php

--- view-doctors.php
+++ view-doctors.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1>Doctors</h1>
+  </div>
+  <div class = "col-auto">
+  <?php
+    include "view-doctors-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
