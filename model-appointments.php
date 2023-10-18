f3be2ebc03cac7b09ebed4d212e75d59f0a6c226
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 15:42:05 -0500

    Update model-appointments.php

--- model-appointments.php
+++ model-appointments.php
@@ -12,4 +12,42 @@ function selectAppointments() {
         throw $e;
     }
 }
+function insertAppointments($aroom, $adaytime) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `appointment` (`room`, `day_time`) VALUES (?,?);
+        $stmt->bind_param("ss",$aroom, $adaytime);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}function updateAppointments($aroom, $adaytime, $aid) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `appointment` set `room` = ?, `day_time` = ?, where appointment_id = ?");
+        $stmt->bind_param("ssi",$aroom, $adaytime, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+}function deleteAppointments($aid) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from appointment where appointment_id = ?");
+        $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
