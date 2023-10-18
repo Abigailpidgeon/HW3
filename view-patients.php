669d12f270fad83574a4a2865d8e6578b64ff07e
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 18:38:47 -0500

    Update view-patients.php

--- view-patients.php
+++ view-patients.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1>Patients</h1>
+  </div>
+  <div class = "col-auto">
+  <?php
+    include "view-patients-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
