6cfb706d93587224a3eb3aac2c2bcf45060e7fed
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 19:19:30 -0500

    Update procedures.php

--- procedures.php
+++ procedures.php
@@ -4,6 +4,15 @@ require_once("model-procedures.php");
 
 $pageTitle = "Procedures";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch ($_POST['actionType']){
+  case "Add":
+    insertProcedures($_POST['pName'],$_POST['pDesc']);
+  break;
+}
+}
+
 $procedures = selectProcedures();
 include "view-procedures.php";
 include "view-footer.php";
