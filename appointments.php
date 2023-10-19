79ec3199f51c5b2e1e50c97bf6accd0493585211
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 19:24:42 -0500

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -4,6 +4,15 @@ require_once("model-appointments.php");
 
 $pageTitle = "Appointments";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch ($_POST['actionType']){
+  case "Add":
+    insertProcedures($_POST['aroom'],$_POST['adaytime']);
+  break;
+}
+}
+
 $appointments = selectAppointments();
 include "view-appointments.php";
 include "view-footer.php";
