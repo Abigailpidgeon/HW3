9e7ee9b8e2d5bd79505cb4b8666740447ce10eb4
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 19:23:55 -0500

    Update doctors.php

--- doctors.php
+++ doctors.php
@@ -4,6 +4,15 @@ require_once("model-doctors.php");
 
 $pageTitle = "Doctors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch ($_POST['actionType']){
+  case "Add":
+    insertProcedures($_POST['dName'],$_POST['dcName']);
+  break;
+}
+}
+
 $doctors = selectDoctors();
 include "view-doctors.php";
 include "view-footer.php";
