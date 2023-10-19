7f552968fd9bcd1ef9fe52e5893a83163569affa
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 19:31:05 -0500

    Update patients.php

--- patients.php
+++ patients.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
 switch ($_POST['actionType']){
   case "Add":
-    insertProcedures($_POST['ppatname'],$_POST['pinname']);
+    if (insertProcedures($_POST['ppatname'],$_POST['pinname'])){
+      echo '<div class="alert alert-success" role="alert"> Procedure added!</div>"';
+        }else{
+     echo  '<div class="alert alert-danger" role="alert">Error....</div>"';
+        }
   break;
 }
 }
