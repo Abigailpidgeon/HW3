9b108eee7d8e110514900fb342963dc012dd985a
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-18 19:22:15 -0500

    Update patients.php

--- patients.php
+++ patients.php
@@ -4,6 +4,15 @@ require_once("model-patients.php");
 
 $pageTitle = "Patients";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+switch ($_POST['actionType']){
+  case "Add":
+    insertProcedures($_POST['ppatname'],$_POST['pinname']);
+  break;
+}
+}
+
 $patients = selectPatients();
 include "view-patients.php";
 include "view-footer.php";
