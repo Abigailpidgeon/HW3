a16281543c74a72dd3610fa838e770e173877996
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-23 13:37:15 -0500

    Update procedures.php

--- procedures.php
+++ procedures.php
@@ -14,6 +14,13 @@ switch ($_POST['actionType']){
      echo  '<div class="alert alert-danger" role="alert">Error....</div>';
         }
   break;
+  case "Delete":
+    if (deleteProcedures($_POST['pid'])){
+      echo '<div class="alert alert-success" role="alert"> Procedure deleted!</div>';
+        }else{
+     echo  '<div class="alert alert-danger" role="alert">Error....</div>';
+        }
+  break;
 }
 }
 
