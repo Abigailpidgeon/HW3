151f8c9d755acf5bccbdf626a328623b18238f55
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-25 14:21:01 -0500

    Update model-appointments.php

--- model-appointments.php
+++ model-appointments.php
@@ -12,6 +12,45 @@ function selectAppointments() {
         throw $e;
     }
 }
+function selectDoctorsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT doctor_id, doctor_name FROM `doctor` order by doctor_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function selectProceduresForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT procedure_id, procedure_name FROM `procedure` order by procedure_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function selectPatientsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT patient_id, patient_name FROM `patient` order by patient_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 function insertAppointments($did,$pid,$ppatid,$aroom,$adaytime) {
    try {
         $conn = get_db_connection();
