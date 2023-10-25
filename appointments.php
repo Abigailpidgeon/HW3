986540becedeef11c4eb5b5ed98e7668a3bd703e
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-10-25 12:34:14 -0500

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
 switch ($_POST['actionType']){
   case "Add":
-   if (insertAppointments($_POST['aroom'],$_POST['adaytime'])){
+   if (insertAppointments($_POST['did'],$_POST['pid'],$_POST['ppatid'],$_POST['aroom'],$_POST['adaytime'])){
       echo '<div class="alert alert-success" role="alert"> Appointment added!</div>';
         }else{
      echo  '<div class="alert alert-danger" role="alert">Error....</div>';
         }
   break;
   case "Edit":
-   if (updateAppointments($_POST['aroom'],$_POST['adaytime'],$_POST['aid'])){
+   if (updateAppointments($_POST['did'],$_POST['pid'],$_POST['ppatid'],$_POST['aroom'],$_POST['adaytime'],$_POST['aid'])){
       echo '<div class="alert alert-success" role="alert"> Appointment updated!</div>';
         }else{
      echo  '<div class="alert alert-danger" role="alert">Error....</div>';
